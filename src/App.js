2869fe66b7be459d4d9fee1457e94978d7a59475
chloet127 <chloet127@gmail.com>
2021-05-12 23:04:32 -0700

    cleaning up formatting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { useFetch } from "./hooks/useFetch";
 import { Route, Switch, Redirect, NavLink } from 'react-router-dom';
 import './style/style.css'
 
@@ -7,9 +6,6 @@ import Assignment2 from './Assignment2';
 import Assignment3 from './Assignment3';
 
 const App = () => {
-  // const [data, loading] = useFetch(
-  //     "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-21/spotify_songs.csv"
-  // );
 
   return (
     <div className='App'>
@@ -20,9 +16,9 @@ const App = () => {
       <main>
         <div>
           <Switch>
-            <Route exact path="/"> <Assignment2 /> </Route>
-            <Route path="/assignment3"> <Assignment3 /> </Route>
-            <Redirect to="/" />
+            <Route exact path="/info474-project"> <Assignment2 /> </Route>
+            <Route path="/info474-project/assignment3"> <Assignment3 /> </Route>
+            <Redirect to="/info474-project" />
           </Switch>
         </div>
       </main>
@@ -35,12 +31,12 @@ function NavigationBar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light">
       <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/">
+        <NavLink className="navbar-brand" to="/info474-project">
           INFO 474 
         </NavLink>
         <div className="navbar-nav">
-          <NavLink className="nav-link" exact to="/" >Assignment 2</NavLink>
-          <NavLink className="nav-link" to="/assignment3" >Assignment 3</NavLink>
+          <NavLink className="nav-link" exact to="/info474-project" >Assignment 2</NavLink>
+          <NavLink className="nav-link" to="/info474-project/assignment3" >Assignment 3</NavLink>
         </div>
       </div>
     </nav>
